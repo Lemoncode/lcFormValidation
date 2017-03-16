ef1993a9c29fe0dd93f00f8a2d7bdbef9884ee11
Santiago Camargo <santi.camargo@lemoncode.net>
2017-03-16 13:12:36 +0100

    Added required rule

---
+++ lib/src/rules/required.ts
@@ -0,0 +1,30 @@
+import { FieldValidationResult, FieldValidationFunction } from '../entities';
+export interface RequiredParams {
+  trim: boolean;
+}
+
+const DEFAULT_PARAMS: RequiredParams = { trim: false };
+
+export const requiredField: FieldValidationFunction = (value, vm, customParams: RequiredParams = DEFAULT_PARAMS) => {
+  const validationResult = new FieldValidationResult();
+  const isValid = isValidField(value, Boolean(customParams.trim));
+  if (!isValid) {
+    validationResult.errorMessage = 'Please fill in this mandatory field.';
+  }
+  validationResult.succeeded = isValid;
+  validationResult.type = 'REQUIRED';
+  return validationResult;
+}
+
+function isValidField(value, trim: boolean): boolean {
+  if (typeof value === 'string') {
+    return isStringValid(value, trim);
+  }
+
+  // Allow only true value
+  return value !== null && value !== undefined && value !== false;
+}
+
+function isStringValid(value: string, trim: boolean): boolean {
+  return trim ? value.trim().length > 0 : value.length > 0;
+}
