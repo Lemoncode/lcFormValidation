6a84e97432accbf7c43fd8fb3056ebc56179ae6b
Santiago Camargo <santi.camargo@lemoncode.net>
2017-03-30 14:02:06 +0200

    Minor fixes.

--- samples/react/es6/02 QuizForm/src/index.jsx
+++ samples/react/es6/02 QuizForm/src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import {render} from 'react-dom';
+import ReactDOM from 'react-dom';
 import App from './components/app';
 
-render(
+ReactDOM.render(
   <App />,
   document.getElementById('root'),
 );
